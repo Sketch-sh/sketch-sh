de7bffad2c70dfbf82522cdbadf02999eedcd63c
Khoa Nguyen <hi@khoanguyen.me>
2018-08-04 23:52:40 +0700

    client: Fix entry.js relocation

--- client/entry.js
+++ client/entry.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import App from "./App.bs";
+import App from "./src/App.bs";
 
 const render = Component => {
   ReactDOM.render(
@@ -14,7 +14,7 @@ render(App);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept("./App.bs", () => {
+  module.hot.accept("./src/App.bs", () => {
     render(App);
   });
 }
