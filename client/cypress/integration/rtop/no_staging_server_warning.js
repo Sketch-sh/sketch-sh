879bdfb28c491fa3d4976705f49475b6162620c1
Khoa Nguyen <hi@khoanguyen.me>
2018-08-21 14:49:56 +0700

    Remove hardcoded localhost url

--- client/cypress/integration/rtop/no_staging_server_warning.js
+++ client/cypress/integration/rtop/no_staging_server_warning.js
@@ -1,14 +1,6 @@
 const faker = require("faker");
 
-let baseUrl = "http://localhost:3000/";
-let url = path => baseUrl + path;
-
 context("no staging server warning", () => {
-  it("don't show warning on localhost", () => {
-    cy.visit(url(""));
-    cy.get(".warning-staging-server").should("not.exist");
-  });
-
   it("show warning on staging host", () => {
     cy.visit("https://staging.sketch.sh");
     cy.get(".warning-staging-server").should("be.visible");
