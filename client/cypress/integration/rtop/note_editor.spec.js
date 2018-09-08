208987316d2583f30c59e51aa5bf2932984d6a5d
Khoa Nguyen <hi@khoanguyen.me>
2018-09-08 21:43:36 +0700

    Add e2e tests

--- client/cypress/integration/rtop/note_editor.spec.js
+++ client/cypress/integration/rtop/note_editor.spec.js
@@ -1,5 +1,19 @@
 /// <reference types="Cypress" />
 
+import {
+  assertBlocks,
+  assertCodeBlocks,
+  assertTextBlocks,
+  assertStdout,
+  assertErrorsOrWarnings,
+  assertValue,
+  shortcut,
+  assertLastBlockValue,
+  typeTitle,
+  typeBlock,
+  aliasSaveButton,
+} from "../../helpers/editor_helpers";
+
 context("Note editor setup", () => {
   it("should not have smart indent", () => {
     cy.visit("new/reason");
@@ -20,5 +34,51 @@ context("Note editor setup", () => {
 };
 a`);
     });
+  });
+});
+
+context("Foldable value", () => {
+  it("should be foldable with large value", () => {
+    cy.visit("new/reason");
+
+    typeBlock(0, "include Pervasives;");
+    shortcut("{ctrl}{enter}");
+
+    assertValue(1);
+    cy.get(".widget__value")
+      .eq(0)
+      .find("button")
+      .should("be.visible");
+
+    cy.get(".widget__foldable--show-gutter")
+      .as("toggleFold")
+      .should("exist")
+      .click();
+    assertValue(1);
+    cy.get(".widget__value")
+      .eq(0)
+      .find("button")
+      .should("not.be.visible");
+
+    cy.get("@toggleFold").click({ force: true });
+    cy.get(".widget__value")
+      .eq(0)
+      .find("button")
+      .should("be.visible");
+  });
+  it("should not display gutter with small value", () => {
+    cy.visit("new/reason");
+
+    typeBlock(0, "let a = 1;");
+    shortcut("{ctrl}{enter}");
+
+    assertValue(1);
+
+    cy.get(".widget__value")
+      .eq(0)
+      .find("button")
+      .should("not.exist");
+
+    cy.get(".widget__foldable--show-gutter").should("not.exist");
   });
 });
