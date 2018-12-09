d3bf90f5046e52db2ba219df28c1fc967ec3364d
Khoa Nguyen <hi@khoanguyen.me>
2018-12-10 02:11:30 +0700

    Fix package name

--- client/public/packages/base.loader.sketch.js
+++ client/public/packages/base.loader.sketch.js
@@ -1,4 +1,8 @@
-importScripts("https://libraries.sketch.sh/base.caml.lib.sketch.js"); sketch__private__base.caml(self);
-importScripts("https://libraries.sketch.sh/base.shadow_stdlib.lib.sketch.js"); sketch__private__base.shadow_stdlib(self);
-importScripts("https://libraries.sketch.sh/sexplib0.lib.sketch.js"); sketch__private__sexplib0(self);
-importScripts("https://libraries.sketch.sh/base.lib.sketch.js"); sketch__private__base(self);
+importScripts("https://libraries.sketch.sh/base_caml.lib.sketch.js");
+sketch__private__base_caml(self);
+importScripts("https://libraries.sketch.sh/base_shadow_stdlib.lib.sketch.js");
+sketch__private__base_shadow_stdlib(self);
+importScripts("https://libraries.sketch.sh/sexplib0.lib.sketch.js");
+sketch__private__sexplib0(self);
+importScripts("https://libraries.sketch.sh/base.lib.sketch.js");
+sketch__private__base(self);
