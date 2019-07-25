c5fd92671a5810d157f9c6ba30cfa65643124b9f
Khoa Nguyen <hi@khoanguyen.me>
2019-07-25 17:26:51 +0700

    deploy: Added 'deploy.sh'
    - Handle config versioning

---
+++ server/deploy.sh
@@ -0,0 +1,22 @@
+#/bin/bash -e
+
+tmp=$(mktemp)
+
+hash () {
+  md5sum $1 | tr ' ' '\n' | head -1
+}
+
+hash_traefik=$(hash "./traefik.toml")
+
+export $(cat .env.production)
+TRAEFIK_CONF=$hash_traefik docker-compose -f docker-compose.prod.yml config > $tmp
+
+echo "Written temporary configuration to $tmp"
+
+eval $(docker-machine env sketch-prod-1)
+
+echo "Switching docker_host to sketch-prod-1"
+
+docker stack deploy -c $tmp sketch
+
+echo "Deployed new stack"
