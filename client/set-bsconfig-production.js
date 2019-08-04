b8da375e4849dc1f756b982a622f39613ca22217
Khoa Nguyen <hi@khoanguyen.me>
2019-08-04 23:30:13 +0700

    ci: only remove -bs-g in bsc-flags

--- client/set-bsconfig-production.js
+++ client/set-bsconfig-production.js
@@ -4,7 +4,7 @@ const bsconfigPath = require.resolve("./bsconfig.json");
 const originalConfig = require(bsconfigPath);
 
 let modifiedConfig = Object.assign({}, originalConfig, {
-  "bsc-flags": undefined,
+  "bsc-flags": originalConfig["bsc-flags"].filter(a => a !== "-bs-g"),
 });
 
 const { writeFileSync } = require("fs");
