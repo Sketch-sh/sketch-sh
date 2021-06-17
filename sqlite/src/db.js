82af2b2b8550f10925e8fc3f362b42db9010ec8d
Khoa Nguyen <hi@khoanguyen.me>
2021-06-17 22:29:34 +0700

    typescript thingy

--- sqlite/src/db.js
+++
@@ -1,8 +0,0 @@
-const { join } = require("path");
-
-const dbPath = join(__dirname, "../data/sketch_without_revision.db");
-const db = require("better-sqlite3")(dbPath, { verbose: console.log });
-
-db.exec("pragma foreign_keys = on;");
-
-module.exports = db;
